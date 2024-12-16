db253aea7882eaf745ba4324e7f67a388f14dbf5
Christopher <christopher.mueller@uni-a.de>
2024-12-16 17:23:33 +0100

    added sender2

--- Receiver2/Receiver2.ino
+++ Receiver2/Receiver2.ino
@@ -14,9 +14,8 @@ double f_wait = 0;
 int bufferSize = 512;
 byte inputBuffer[512];
 
-int stopSize = 24;
-byte stopCode[24];
-
+int stopSize = 25;
+byte stopCode[25];
 
 int getLstate(int threash) {
   if ((analogRead(DETECT_PIN) >= threash)) {
@@ -27,11 +26,13 @@ int getLstate(int threash) {
 }
 
 char *decode(int idx) {
+  // interprete message without stop code
   byte inputMsg[idx];
   for (int i = 0; i <= idx; i++) {
     inputMsg[i] = inputBuffer[i];
   }
 
+  // turn bits into characters
   int charlen = sizeof(inputMsg) / 8;
   char msg[charlen];
   int i = 0;
@@ -46,6 +47,7 @@ char *decode(int idx) {
 }
 
 int findMatchIndex(byte* inptArr) {
+  // find index where input array matches the predefined stopcode 
   for (int i = 0; i <= bufferSize - stopSize; i++) {
     bool match = true;
     for (int j = 0; j < stopSize; j++) {
@@ -58,51 +60,45 @@ int findMatchIndex(byte* inptArr) {
       return i;  
     }
   }
+  // return -1 when unmatched
   return -1;
 }
 
 void setup() {
   pinMode(13, OUTPUT);
   Serial.begin(9600);
-  stopCode[0] = 0;
-  stopCode[1] = 0;
-  stopCode[2] = 0;
-  stopCode[3] = 0;
-  stopCode[4] = 0;
 
-  stopCode[5] = 1;
-  stopCode[6] = 0;
-
-  stopCode[7] = 1;
-  stopCode[8] = 0;
-
-  stopCode[9] = 1;
+  // 0 0 0 0 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1
+  stopCode[0]  = 0;
+  stopCode[1]  = 0;
+  stopCode[2]  = 0;
+  stopCode[3]  = 0;
+  stopCode[4]  = 0;
+  stopCode[5]  = 1;
+  stopCode[6]  = 0;
+  stopCode[7]  = 1;
+  stopCode[8]  = 0;
+  stopCode[9]  = 1;
   stopCode[10] = 0;
-
   stopCode[11] = 1;
   stopCode[12] = 0;
-
   stopCode[13] = 1;
   stopCode[14] = 0;
-
   stopCode[15] = 1;
   stopCode[16] = 0;
-
   stopCode[17] = 1;
   stopCode[18] = 0;
-
   stopCode[19] = 1;
   stopCode[20] = 0;
-
   stopCode[21] = 1;
   stopCode[22] = 0;
-
   stopCode[23] = 1;
+  stopCode[24] = 0;
 
-  //[0 0 0 0 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1]
 }
 
 void loop() {
+  // detect start code of possible data transmission
   int onCounter = 0;
   double timeElapsed = 0;
   while (onCounter != 10) {
@@ -120,15 +116,19 @@ void loop() {
       onCounter = 0;
     }
   }
+
+  // receive data package
   digitalWrite(13, HIGH);
   onTime = onTime / 10;
-  delay(onTime * 5);
+  delay(onTime * 4);
   int i = 0;
   while (i < bufferSize) {
     inputBuffer[i] = getLstate(detectThreash);
     delay(onTime);
   }
   digitalWrite(13, LOW);
+
+  // read binary message
   int indx = findMatchIndex(inputBuffer);
   if (indx == -1) {
     Serial.println("Error: Buffer overflown or package lost");
---
+++ Sender2/Sender2.ino
@@ -0,0 +1,104 @@
+#define LASER_PIN 10
+
+int waitMillis = 10;
+double frequency = 1 / (waitMillis / 1000);
+
+byte startCode[24];
+byte stopCode[25];
+
+void laser(int state) {
+  digitalWrite(LASER_PIN, state);
+}
+
+void sendMsg(String sInput) {
+  // typeconversion String -> char*
+  int inputLen = sInput.length();
+  char *cInput = (char*)malloc(inputLen * sizeof(char));
+  sInput.toCharArray(cInput, inputLen);
+
+  // OOK
+  digitalWrite(13, HIGH);
+  int bits[sizeof(cInput) * 8];
+  // sent startcode
+  for (int i = 0; i < 24 * 8; i++) {
+    laser(startCode[i]);
+    delay(waitMillis);
+  }
+  // send data package
+  int j = 0;
+  for (int i = 0; i < sizeof(cInput) * 8; i++) {
+    j = floor(i / 8);
+    laser(bitRead(cInput[j], i % 8));
+    delay(waitMillis);
+  }
+  // sent stopcode
+  for (int i = 0; i < 24 * 8; i++) {
+    laser(stopCode[i]);
+    delay(waitMillis);
+  }
+  digitalWrite(13, LOW);
+  free(cInput);
+}
+
+void setup() {
+  pinMode(LASER_PIN, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  startCode[0]  = 1;
+  startCode[1]  = 0;
+  startCode[2]  = 1;
+  startCode[3]  = 0;
+  startCode[4]  = 1;
+  startCode[5]  = 0;
+  startCode[6]  = 1;
+  startCode[7]  = 0;
+  startCode[8]  = 1;
+  startCode[9]  = 0;
+  startCode[10] = 1;
+  startCode[11] = 0;
+  startCode[12] = 1;
+  startCode[13] = 0;
+  startCode[14] = 1;
+  startCode[15] = 0;
+  startCode[16] = 1;
+  startCode[17] = 0;
+  startCode[18] = 1;
+  startCode[19] = 0;
+  startCode[20] = 0;
+  startCode[21] = 0;
+  startCode[22] = 0;
+  startCode[23] = 0;
+
+  stopCode[0]  = 0;
+  stopCode[1]  = 0;
+  stopCode[2]  = 0;
+  stopCode[3]  = 0;
+  stopCode[4]  = 0;
+  stopCode[5]  = 1;
+  stopCode[6]  = 0;
+  stopCode[7]  = 1;
+  stopCode[8]  = 0;
+  stopCode[9]  = 1;
+  stopCode[10] = 0;
+  stopCode[11] = 1;
+  stopCode[12] = 0;
+  stopCode[13] = 1;
+  stopCode[14] = 0;
+  stopCode[15] = 1;
+  stopCode[16] = 0;
+  stopCode[17] = 1;
+  stopCode[18] = 0;
+  stopCode[19] = 1;
+  stopCode[20] = 0;
+  stopCode[21] = 1;
+  stopCode[22] = 0;
+  stopCode[23] = 1;
+  stopCode[24] = 0;
+}
+
+void loop() {
+  if (Serial.available()) {
+    String userInput = Serial.readStringUntil('\n');
+    sendMsg(userInput);
+  }
+}
